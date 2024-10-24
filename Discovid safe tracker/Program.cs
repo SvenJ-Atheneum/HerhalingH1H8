86ac3e9a9ff4d3ad94d53a487472befceb7aa790
SvenJ-Atheneum <sven.janssens@atheneumsinttruiden.be>
2024-10-24 16:05:53 +0200

    eerste commit

--- Discovid safe tracker/Program.cs
+++ Discovid safe tracker/Program.cs
@@ -6,7 +6,7 @@
         bool[] toegelaten = new bool[100];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hallo, Wereld");
         }
     }
 }
