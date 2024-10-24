390c21cb16a57b7189f36a3eb60d73268ecb408d
SvenJ-Atheneum <sven.janssens@atheneumsinttruiden.be>
2024-10-24 16:02:23 +0200

    Add project files.

---
+++ Discovid safe tracker/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Discovid_safe_tracker
+{
+    internal class Program
+    {
+        string[] namenlijst = new string[100];
+        bool[] toegelaten = new bool[100];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
